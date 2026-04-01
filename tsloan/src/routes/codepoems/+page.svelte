b7745c87dd6ecb8018379da4f7e20a6601a72b38
Taylor Sloan <taylorsloan3430@gmail.com>
2026-04-01 16:03:36 -0500

    Added code poems

---
+++ tsloan/src/routes/codepoems/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+</script>
+
+<svelte:head>
+	<title>Code Poems</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Code Poems</h1>
+	<p><a href="documents/codepoems.pdf">Link to pdf of project</a></p>
+	<p><a href="documents/englishSymposium.pptx">Link to presentation</a> <a href="documents/englishSymposium.pdf">(PDF)</a></p>
+</div>
+
+<style>
+	.text-column{
+		text-align: center;
+		p{
+			margin: 0;
+		}
+	}
+</style>
